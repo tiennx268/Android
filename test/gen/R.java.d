47635723001f475c8b1539d7b374e0f2ac469501
tiennx268 <ntien268@gmail.com>
2013-10-01 09:44:00 +0700

    test

---
+++ test/gen/R.java.d
@@ -0,0 +1,8 @@
+D:\Project Mobile\test\gen\wow\test\R.java \
+ : D:\Project Mobile\test\res\drawable-hdpi\ic_launcher.png \
+D:\Project Mobile\test\res\drawable-ldpi\ic_launcher.png \
+D:\Project Mobile\test\res\drawable-mdpi\ic_launcher.png \
+D:\Project Mobile\test\res\drawable-xhdpi\ic_launcher.png \
+D:\Project Mobile\test\res\layout\main.xml \
+D:\Project Mobile\test\res\values\strings.xml \
+D:\Project Mobile\test\bin\AndroidManifest.xml \
